29b7949ddf171fc1c2ec38770ee55bd1124c3f70
Rohan-V <rohankvij@gmail.com>
2022-06-06 12:06:50 -0700

    Fixed pylint error

--- testing.py
+++ testing.py
@@ -7,7 +7,9 @@ config = load_file("config.yaml")
 CATEGORIES = config["CATEGORIES"]
 IMAGE_PATTERNS = config["PATTERNS"]["IMAGES"]
 
-file_names = ["7123-some-example-txt-detail-high.jpg", "7123-some-example-txt-detail.jpg", "7123-some-example-txt-detail-low.jpg"]
+file_names = ["7123-some-example-txt-detail-high.jpg",
+              "7123-some-example-txt-detail.jpg",
+              "7123-some-example-txt-detail-low.jpg"]
 
 for file_name in file_names:
     print(file_name)
